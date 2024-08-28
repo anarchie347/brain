ca60ed6f573d3b7e0c7d088044400ea19c0386f5
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-08-28 22:55:55 +0100

    began writing brain32 translator

--- src/translators/brain32.rs
+++ src/translators/brain32.rs
@@ -1,4 +1,52 @@
 
-pub fn translate(source : String) -> String {
+pub fn translate(source : Vec<CodeBlock>) -> String {
 
+
+    "".to_string()
 }
+
+
+enum Mode {
+    Data,Working,Raw
+}
+struct CodeBlock {
+    mode : Mode,
+    code: Vec<Instruction>
+}
+struct Instruction{
+    parameter: u8,
+    name: char
+}
+
+fn translate_character(c: char, mode : Mode) -> &str {
+    match mode {
+        Mode::Data => translate_character_data,
+        Mode::Working => translate_character_working
+        Mode::Raw => |x : char| -> &str {""}
+    }(c)
+}
+
+fn translate_character_data(c: char) -> &str {
+    match c {
+        '+' => data_add,
+        '-' => data_sub,
+        '[' => data_open,
+        ']' => data_close,
+        ',' => ",",
+        '.' => ".",
+        '>' => ">>>>",
+        '<' => "<<<<",
+        _ => ""
+    }
+}
+
+fn translate_character_working(c: char) -> &str {
+    ""
+}
+
+
+
+const data_add: &str = "a";
+const data_sub: &str = "b";
+const data_open: &str = "c";
+const data_close: &str = "d";
