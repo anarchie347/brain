8a6452c7abf8f7118b10ce1eef31ffc68e278d9f
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-09-01 12:05:37 +0100

    added move method constants

--- src/translators/brain32.rs
+++ src/translators/brain32.rs
@@ -69,9 +69,21 @@ fn translate_instruction_working(i: &Instruction) -> Vec<char> {
     ).chars().collect()
 }
 
+// More complex substitution values
 
-
+//basic data operations
 const DATA_ADD: &str = "a";
 const DATA_SUB: &str = "b";
 const DATA_OPEN: &str = "c";
 const DATA_CLOSE: &str = "d";
+
+//move
+const DATA_MOVE_FROM_0: &str = "[<<<<+>>>>-]"; //all start and end at D0, and move to W0
+const DATA_MOVE_FROM_1: &str = "<[<<<+>>>-]>";
+const DATA_MOVE_FROM_2: &str = "<<[<<+>>-]>>";
+const DATA_MOVE_FROM_3: &str = "<<<[<+>-]>>>";
+
+const WORKING_MOVE_TO_0: &str = "[>>>>+<<<<-]"; //all start and end in W0, move to Dx
+const WORKING_MOVE_TO_1: &str = "[>>>+<<<-]";
+const WORKING_MOVE_TO_2: &str = "[>>+<<-]";
+const WORKING_MOVE_TO_3: &str = "[>+<-]";
