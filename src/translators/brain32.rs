9e225aafa21ead8b9554052c552db02f090b6df1
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-09-15 20:03:58 +0100

    added note to gen macro

--- src/translators/brain32.rs
+++ src/translators/brain32.rs
@@ -91,6 +91,7 @@ const fn substitution_data_move_from(cell: u8) -> &'static str {
 }
 
 macro_rules! gen_if_zero_working {
+    //use concat rather than format for compile time computtion
     ($zero:expr, $non_zero:expr) => {
         format!(
             ">>>>>[-]
