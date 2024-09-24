9eedf64a89b5267f93383ac0b412e2444c2237b0
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-09-24 17:14:29 +0100

    reimplemented some brain32 substitution logic

--- src/translators/brain32.rs
+++ src/translators/brain32.rs
@@ -1,45 +1,50 @@
-pub fn translate(source : Vec<CodeBlock>) -> String {
+pub fn translate(source: Vec<CodeBlock>) -> String {
     source.iter().flat_map(translate_block).collect()
 }
 
-
-#[derive (Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 #[allow(dead_code)]
 pub enum Mode {
-    Data,Working,Raw
+    Data,
+    Working,
+    Raw,
 }
 pub struct CodeBlock {
-    pub mode : Mode,
-    pub code: Vec<Instruction>
+    pub mode: Mode,
+    pub code: Vec<Instruction>,
 }
-pub struct Instruction{
+pub struct Instruction {
     pub parameter: u8,
-    pub name: char
+    pub name: char,
 }
 
-fn translate_block(block : &CodeBlock) -> Vec<char> {
+fn translate_block(block: &CodeBlock) -> Vec<char> {
     let mode = block.mode;
-    let mut translated: Vec<char> = block.code.iter().flat_map(|i| translate_instruction(i, mode)).collect();
-    if mode == Mode::Working { //W blocks require wrapping in <<< code >>> to move in/out of W memory
+    let mut translated: Vec<char> = block
+        .code
+        .iter()
+        .flat_map(|i| translate_instruction(i, mode))
+        .collect();
+    if mode == Mode::Working {
+        //W blocks require wrapping in <<< code >>> to move in/out of W memory
         let mut full = vec!['<'; 4];
         full.append(&mut translated);
         full.append(&mut vec!['>'; 4]);
-        return full;      
+        return full;
     }
     translated
 }
 
-fn translate_instruction(i: &Instruction, mode : Mode) -> Vec<char> {
+fn translate_instruction(i: &Instruction, mode: Mode) -> Vec<char> {
     (match mode {
         Mode::Data => translate_instruction_data,
         Mode::Working => translate_instruction_working,
-        Mode::Raw => |x : &Instruction| -> Vec<char> {vec![x.name]}
+        Mode::Raw => |x: &Instruction| -> Vec<char> { vec![x.name] },
     })(i)
 }
 
 fn translate_instruction_data(i: &Instruction) -> Vec<char> {
-    String::from(
-        match i.name {
+    String::from(match i.name {
         '+' => DATA_ADD,
         '-' => DATA_SUB,
         '[' => DATA_OPEN,
@@ -48,24 +53,26 @@ fn translate_instruction_data(i: &Instruction) -> Vec<char> {
         '.' => ".",
         '>' => ">>>>>",
         '<' => "<<<<<",
-        _ => ""
-    }).chars().collect()
+        _ => "",
+    })
+    .chars()
+    .collect()
 }
 
 fn translate_instruction_working(i: &Instruction) -> Vec<char> {
-    String::from(
-        match i.name {
-            '+' => "+",
-            '-' => "-",
-            '[' => "[",
-            ']' => "]",
-            ',' => ",",
-            '.' => ".",
-            '>' => ">>>>>",
-            '<' => "<<<<<",
-            _ => ""
-        }
-    ).chars().collect()
+    String::from(match i.name {
+        '+' => "+",
+        '-' => "-",
+        '[' => "[",
+        ']' => "]",
+        ',' => ",",
+        '.' => ".",
+        '>' => ">>>>>",
+        '<' => "<<<<<",
+        _ => "",
+    })
+    .chars()
+    .collect()
 }
 
 // More complex substitution values
@@ -75,34 +82,77 @@ const DATA_ADD: &str = "a";
 const DATA_SUB: &str = "b";
 const DATA_OPEN: &str = "c";
 const DATA_CLOSE: &str = "d";
+const ZERO: &str = "[-]";
 
 //move
 
-//all start and end at D0, and move to W0
-macro_rules! data_move_from {
-    (0) => {"<<<<[-]>>>>[<<<<+>>>>-]"};
-    (1) => {"<<<<[-]>>>[<<<+>>>-]>"};
-    (2) => {"<<<<[-]>>[<<+>>-]>>"};
-    (3) => {"<<<<[-]>[<+>-]>>>"};
-    ($x:expr) => {compile_error!("arg must be 0,1,2 or 3")};
+macro_rules! ptr_move_to_data {
+    (0) => {
+        ">>>>"
+    };
+    (1) => {
+        ">>>"
+    };
+    (2) => {
+        ">>"
+    };
+    (3) => {
+        ">"
+    };
+    ($x:expr) => {
+        compile_error!("arg must be 0,1,2 or 3")
+    };
+}
+macro_rules! ptr_move_to_working {
+    (0) => {
+        "<<<<"
+    };
+    (1) => {
+        "<<<"
+    };
+    (2) => {
+        "<<"
+    };
+    (3) => {
+        "<"
+    };
+    ($x:expr) => {
+        compile_error!("arg must be 0,1,2 or 3")
+    };
 }
 
-macro_rules! gen_if_zero_working {
+//all start and end at D0, and move to W0
+macro_rules! data_move_from {
+    ($offset:expr) => {
+        concat!(
+            ptr_move_to_working!(offset),
+            ZERO,
+            ptr_move_to_data!(offset),
+            "[",
+            ptr_move_to_working!(offset),
+            "+",
+            ptr_move_to_data!(offset),
+            "-]"
+        )
+    }
+}
+
+macro_rules! _if_zero_working {
     ($zero:expr, $non_zero:expr) => {
         concat!(
             ">>>>>[-]",
             ">>>>>[-]-",
             "<<<<< <<<<<",
             "[",
-                non_zero,
-                ">>>>>",
+            non_zero,
+            ">>>>>",
             "]",
             ">>>>>",
             "+",
             "[",
-                "<<<<<",
-                zero,
-                ">>>>> >>>>> +",
+            "<<<<<",
+            zero,
+            ">>>>> >>>>> +",
             "]",
             "<<<<< <<<<<",
         );
@@ -110,10 +160,16 @@ macro_rules! gen_if_zero_working {
 }
 //all start and end in W0, move to Dx
 macro_rules! working_move_to {
-    (0) => {">>>>[-]<<<<[>>>>+<<<<-]"};
-    (1) => {">>>[-]<<<[>>>+<<<-]"};
-    (2) => {">>[-]<<[>>+<<-]"};
-    (3) => {">[-]<[>+<-]"};
-    ($x:expr) => {compile_error!("arg must be 0,1,2 or 3")};
+    ($offset:expr) => {
+        concat!(
+            ptr_move_to_data(offset),
+            ZERO,
+            ptr_move_to_working(offset),
+            "[",
+            ptr_move_to_data(offset),
+            "+",
+            ptr_move_to_working(offset),
+            "-]"
+        )
+    };
 }
-
