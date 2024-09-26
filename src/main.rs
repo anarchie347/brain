c114872b9dd4e81b0f807adc3fc50e781d02d948
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-09-26 13:58:36 +0100

    refactored code in/out system

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ mod brainfuckinterpreter;
 mod translators;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
     //let source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
     //let source = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.-.";
-    let source = "+#@";
+    //let source = "+#@";
+    let source = read_from_file(&String::from("in.txt"));
 
     let mut source_vec: Vec<(Mode, String)> = Vec::new();
     source_vec.push((Mode::Data, source.to_string()));
     let parsed = translators::brain32::parse(source_vec);
     let brainfuck = translators::brain32::translate(parsed);
-    write_to_file(&brainfuck, &args[1]);
+    write_to_file(&brainfuck, &String::from("out.txt"));
     //println!("{}", brainfuck);
     run(&brainfuck);
 }
@@ -24,4 +25,9 @@ fn main() {
 fn write_to_file(s: &String, path: &String) {
     let mut output = std::fs::File::create(path).unwrap();
     output.write_all(s.as_bytes());
+}
+
+fn read_from_file(path: &String) -> String {
+    let contents = std::fs::read(path).expect("Coundlt read file");
+    String::from_utf8(contents).expect("Couldnt convert file contents to string")
 }
