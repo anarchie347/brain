ad148917cfc4ca18fe04486faaa261c259bca0cb
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2024-09-26 12:03:43 +0100

    added testing code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::{env, io::Write};
+
 use translators::brain32::{CodeBlock, Instruction, Mode};
 
 mod translators;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     //let c = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    //let c = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.-.";
     let c = "+";
     let block = CodeBlock {
         mode: Mode::Data,
@@ -18,5 +22,11 @@ fn main() {
     let mut v: Vec<CodeBlock> = Vec::new();
     v.push(block);
     let s = translators::brain32::translate(v);
+    write_to_file(&s, &args[1]);
     println!("{}", s);
+}
+
+fn write_to_file(s: &String, path: &String) {
+    let mut output = std::fs::File::create(path).unwrap();
+    output.write_all(s.as_bytes());
 }
