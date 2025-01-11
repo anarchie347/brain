398f0e143afb6ad9e2417d8623df6d4ddf96c57b
Anarchie347 <116115028+Anarchie347@users.noreply.github.com>
2025-01-11 20:28:27 +0000

    implemented (basic, not fully finished) brain32 translation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{env, io::Write};
 
 use brainfuckinterpreter::run;
-use translators::brain32::{CodeBlock, Mode, Token};
 
 mod brainfuckinterpreter;
 mod translators;
@@ -11,15 +10,12 @@ fn main() {
     //let source = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
     //let source = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.+.-.";
     //let source = "+#@";
+
     let source = read_from_file(&String::from("in.txt"));
 
-    let mut source_vec: Vec<(Mode, String)> = Vec::new();
-    source_vec.push((Mode::Raw, String::from_utf8(vec![b'+'; 255]).unwrap()));
-    source_vec.push((Mode::Data, source.to_string()));
-    let parsed = translators::brain32::parse(source_vec);
+    let parsed = translators::brain32::parse(source);
     let brainfuck = translators::brain32::translate(parsed);
     write_to_file(&brainfuck, &String::from("out.txt"));
-    //println!("{}", brainfuck);
     run(&brainfuck);
 }
 
--- src/translators/brain32.rs
+++ src/translators/brain32.rs
@@ -1,92 +1,63 @@
-pub fn translate(source: Vec<CodeBlock>) -> String {
-    let mut full = vec!['>'; 4]; //wraps code in arrows to go to D0 to start,
-    full.append(&mut source.iter().flat_map(translate_block).collect());
-    full.append(&mut vec!['<'; 4]);
-    full.iter().collect()
-}
-
-pub fn parse(source: Vec<(Mode, String)>) -> Vec<CodeBlock> {
+pub fn parse(source: String) -> Vec<Token> {
     source
-        .iter()
-        .map(|(mode, section)| CodeBlock {
-            mode: *mode,
-            code: parse_section(section),
+        .chars()
+        .map(|c| match c {
+            '+' => Token::Plus(None),
+            '-' => Token::Minus(None),
+            '>' => Token::Right,
+            '<' => Token::Left,
+            ',' => Token::Comma(SingleArg::Zero),
+            '.' => Token::Dot(SingleArg::Zero),
+            '[' => Token::Open(Vec::new()),
+            ']' => Token::Close(Vec::new()),
         })
         .collect()
 }
 
-//starts in W0
-const DATA_ADD_FULL: String = String::from(concat!(
-    "[-]>+>+>+>+<<<<<" //zero W0, increment all D
-    data_copy_to_working(0),
-    "[>>>-" //if W0(=D0) != 0, undo D1 carry, end up in D1
-    "<<<[-]" //return to W0 and set 0
-
-
-
-));
-
-macro_rules! ptr_W0_to_Dx {
-    (0) => {
-        "<"
-    };
-    (1) => {
-        "<<"
-    };
-    (2) => {
-        "<<<"
-    };
-    (3) => {
-        "<<<<"
-    };
+pub fn translate(source: Vec<Token>) -> String {
+    source
+        .iter()
+        .map(|t| match t {
+            Token::Plus(_) => TRANSLATION_ADD_FULL,
+            Token::Minus(_) => TRANSLATION_SUB_FULL,
+            Token::Right => TRANSLATION_RIGHT,
+            Token::Left => TRANSLATION_LEFT,
+            Token::Comma(_) => "<,>",
+            Token::Dot(_) => "<.>",
+            Token::Open(_) => TRANSLATION_OPEN_FULL,
+            Token::Close(_) => TRANSLATION_CLOSE_FULL,
+        })
+        .collect()
 }
 
-macro_rules! ptr_Dx_to_W0 {
-    (0) => {
-        ">"
-    };
-    (1) => {
-        ">>"
-    };
-    (2) => {
-        ">>>"
-    };
-    (3) => {
-        ">>>>"
-    };
+pub enum Token {
+    Plus(Option<SingleArg>),
+    Minus(Option<SingleArg>),
+    Right,
+    Left,
+    Comma(SingleArg),
+    Dot(SingleArg),
+    Open(Vec<SingleArg>),
+    Close(Vec<SingleArg>),
 }
 
-macro_rules! move_patterned {
-    ($($x:expr), *) => {
-        let mut move_code = String::from("[");
-        $(
-            let direction = if $x < 0 {'<'} else {'>'};
-            result.push_str(direction.repeat(($x).abs()));
-            result.push('+');
-        )*
-
-        $(
-            let direction = if $x < 0 {'>'} else {'<'};
-            result.push_str(direction.repeat(($x).abs()));
-        )*
-        result.push("-");
-    };
+pub enum SingleArg {
+    Zero,
+    One,
+    Two,
+    Three,
 }
 
-//--------------The following assume the pointer starts in W0 and returns the pointer to W0
+// Unless otherwise annotated, the following hardcoded conversions are generated using the following python scripts, the algorithms used are explained in the python source code: https://github.com/anarchie347/brain-translation-generation-scripts
 
-macro_rules! move_Dx_to_W0 {
-    () => {};
-}
+const TRANSLATION_ADD_FULL: &str = "<+<+<+<+<[-]+>>>>>[-]>>>>>[-]<<<<<<[>+>>>>>+<<<<<<-]>>>>>>[<<<<<<+>>>>>>-]<<<<<[<<-<-<-<[-]>>>>>[-]][-]>>>>>[-]<<<<<<<[>>+>>>>>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<<<<<[<<<<<[>>-<-<[-]]>>>>>[-]][-]>>>>>[-]<<<<<<<<[>>>+>>>>>+<<<<<<<<-]>>>>>>>>[<<<<<<<<+>>>>>>>>-]<<<<<[<<<<<[>-<[-]]>>>>>[-]]";
 
-macro_rules! move_W0_to_Dx {
-    () => {};
-}
+const TRANSLATION_SUB_FULL: &str = "<<<<<[-]+>>>>>[-]>>>>>[-]<<<<<<[>+>>>>>+<<<<<<-]>>>>>>[<<<<<<+>>>>>>-]<<<<<[<<+<+<+<[-]>>>>>[-]][-]>>>>>[-]<<<<<<<[>>+>>>>>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<<<<<[<<<<<[>>+<+<[-]]>>>>>[-]][-]>>>>>[-]<<<<<<<<[>>>+>>>>>+<<<<<<<<-]>>>>>>>>[<<<<<<<<+>>>>>>>>-]<<<<<[<<<<<[>+<[-]]>>>>>[-]]<-<-<-<->>>>";
 
-macro_rules! copy_Dx_to_W0 {
-    () => {};
-}
+const TRANSLATION_RIGHT: &str = ">>>>>"; //cells are grouped in blocks of 5
 
-macro_rules! copy_W0_to_Dx {
-    () => {};
-}
+const TRANSLATION_LEFT: &str = "<<<<<"; //cells are grouped in blocks of 5
+
+const TRANSLATION_OPEN_FULL: &str = "<<<<<[-]>>>>>[-]>>>>>[-]<<<<<<[>+>>>>>+<<<<<<-]>>>>>>[<<<<<<+>>>>>>-]<<<<<[<<<<<+>>>>>[-]][-]>>>>>[-]<<<<<<<[>>+>>>>>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<<<<<[<<<<<+>>>>>[-]][-]>>>>>[-]<<<<<<<<[>>>+>>>>>+<<<<<<<<-]>>>>>>>>[<<<<<<<<+>>>>>>>>-]<<<<<[<<<<<+>>>>>[-]][-]>>>>>[-]<<<<<<<<<[>>>>+>>>>>+<<<<<<<<<-]>>>>>>>>>[<<<<<<<<<+>>>>>>>>>-]<<<<<[<<<<<+>>>>>[-]]<<<<<[>>>>>";
+
+const TRANSLATION_CLOSE_FULL: &str = "<<<<<[-]>>>>>[-]>>>>>[-]<<<<<<[>+>>>>>+<<<<<<-]>>>>>>[<<<<<<+>>>>>>-]<<<<<[<<<<<+>>>>>[-]][-]>>>>>[-]<<<<<<<[>>+>>>>>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<<<<<[<<<<<+>>>>>[-]][-]>>>>>[-]<<<<<<<<[>>>+>>>>>+<<<<<<<<-]>>>>>>>>[<<<<<<<<+>>>>>>>>-]<<<<<[<<<<<+>>>>>[-]][-]>>>>>[-]<<<<<<<<<[>>>>+>>>>>+<<<<<<<<<-]>>>>>>>>>[<<<<<<<<<+>>>>>>>>>-]<<<<<[<<<<<+>>>>>[-]]<<<<<]>>>>>";
